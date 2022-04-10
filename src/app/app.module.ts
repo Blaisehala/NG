8b614419f6424e9e3210cf09c03277c87352bd5f
Blaise Hala <blaisehala@gmail.com>
2022-04-10 12:55:02 +0300

    st:adds background image and styles main page container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotehighlightDirective } from './quotehighlight.directive';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { FormsModule } from '@angular/forms';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
