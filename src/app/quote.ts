14034095a8abb405f03272d928e58a000c38400c
Blaise Hala <blaisehala@gmail.com>
2022-04-10 23:06:22 +0300

    feat:adds upvote and downvote functionality

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,22 @@
 export class Quote {
 
+
   showDescription:boolean;
-  constructor(public id:number, public title:string, public description:string, public author:string, public yourname:string, public postDate:Date = new Date()){
+  votes:number;
+  vote:number;
+  constructor(public id:number, public title:string, public description:string,
+     public author:string, public yourname:string, public postDate:Date = new Date(), 
+     votes:number=0, vote:number=0){
 this.showDescription=false;
+this.votes=votes||0
+this.vote=vote||0
+  }
+  upvote(){
+    this.votes++
+  }
+
+  downvote(){
+    this.vote++
   }
 
  
